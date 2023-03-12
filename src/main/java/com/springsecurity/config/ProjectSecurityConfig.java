c826998e24c388f65f2e432f14a8c531bc3072e7
Julian Zaleski <julizale@gmail.com>
2023-03-12 21:10:07 +0100

    Added BCryptPasswordEncoder

--- src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
@@ -3,7 +3,7 @@ package com.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,26 +21,8 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    /*@Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-    }*/
-
-    /*@Bean
-    public JdbcUserDetailsManager userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
